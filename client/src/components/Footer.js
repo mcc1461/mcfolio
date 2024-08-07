5809ed89ce1af7b74fea610a457935da7c4a706a
mcc1461 <mcoskuncelebi@gmail.com>
2024-08-07 13:00:42 +0300

    feat: Contact and Footer finalized

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -5,9 +5,17 @@ const Footer = () => {
   const formattedDate = currentDate.getFullYear(); // Format the date as needed
 
   return (
-    <div className="text-center h-[7vh] border-t-2 bg-mc-blue-darker1 border-white ">
+    <div className="text-center h-[7vh] border-t-2 bg-mc-blue-darker1 border-white py-3">
+      <p className="italic text-gray-400 text-md">Designed and developed by</p>
       <p className="text-lg text-gray-400">
-        © <span>{formattedDate}</span> MC Portfolio
+        <p>
+          <span className="text-quinary-100">Mus</span>
+          <span>tafa</span> <span>{}</span>
+          <span className="text-quinary-100">Co</span>
+          <span>skuncelebi</span> <span>{}</span>{" "}
+          <span className="text-quinary-100">©</span>{" "}
+          <span>{formattedDate}</span>
+        </p>
       </p>
     </div>
   );
--- client/src/pages/Home/Contact.js
+++ client/src/pages/Home/Contact.js
@@ -34,13 +34,14 @@ const Contact = () => {
       threshold: 0.5, // Adjust this value as needed
     });
 
-    if (videoRef.current) {
-      observer.observe(videoRef.current);
+    const currentVideoRef = videoRef.current;
+    if (currentVideoRef) {
+      observer.observe(currentVideoRef);
     }
 
     return () => {
-      if (videoRef.current) {
-        observer.unobserve(videoRef.current);
+      if (currentVideoRef) {
+        observer.unobserve(currentVideoRef);
       }
     };
   }, []);
@@ -48,60 +49,60 @@ const Contact = () => {
   return (
     <>
       <SectionTitle title="Contact" />
-      {/* <div className="flex items-center justify-center h-full bg-mc-blue">
-        <img src={contactMe} alt="Contact QR" className="h-[40vh] rounded" />
-      </div> */}
-      <div className="flex items-center justify-center h-full py-9 bg-mc-blue">
-        <video
-          ref={videoRef}
-          src={contactVideo}
-          alt="Contact Video"
-          className="h-[40vh] rounded"
-          controls={false}
-          autoPlay={false}
-        />
-      </div>
-      <div
-        className="flex flex-col items-center justify-center h-full bg-mc-blue"
-        onMouseEnter={() => setEntered(true)}
-        onMouseLeave={() => setEntered(false)}
-      >
+      <div className="flex flex-col items-center justify-center h-full gap-7 lg:flex-row lg:gap-2 xl:flex-row xl2:flex-row xl:gap-2 xl2:gap-1 py-9 bg-mc-blue">
+        <div className="flex items-center justify-center w-full outline-dotted outline-yellow-300 lg:w-1/2 xl:w-1/2 xl2:w-1/2 lg:justify-end xl:justify-end xl2:justify-end">
+          <video
+            ref={videoRef}
+            src={contactVideo}
+            alt="Contact Video"
+            className="h-[40vh] lg:h-[30vh] md:h-[26vh] sm:h-[20vh] rounded"
+            controls={false}
+            autoPlay={true}
+            muted
+          />
+        </div>
         <div
-          className={`flex flex-col items-center justify-center p-4 m-4 rounded-lg shadow-lg gap-7 ${
-            entered
-              ? "bg-mc-blue-darker3 text-quaternary-300 border-quaternary-200"
-              : "bg-mc-blue-darker1 text-mc-white border-[#258d54]"
-          } border-l-4 pl-2 `}
+          className="flex flex-col items-center justify-center w-full h-full px-5 bg-mc-blue lg:w-1/2 xl:w-1/2 xl2:w-1/2 lg:items-start xl:items-start xl2:items-start"
+          onMouseEnter={() => setEntered(true)}
+          onMouseLeave={() => setEntered(false)}
         >
-          <a
-            href="https://www.linkedin.com/in/mcoskuncelebi/"
-            target="_blank"
-            rel="noreferrer"
-            className={`flex items-center text-2xl font-semibold ${
+          <div
+            className={`flex flex-col items-center justify-center p-4 rounded-lg shadow-lg gap-4 ${
               entered
-                ? "text-quaternary-300 underline cursor-pointer"
-                : "text-mc-white"
-            }`}
+                ? "bg-mc-blue-darker3 text-quaternary-300 border-quaternary-200"
+                : "bg-mc-blue-darker1 text-mc-white border-[#258d54]"
+            } border-l-4 pl-2`}
           >
-            {user.name}
-            {entered && (
-              <span className="px-2">
-                <button className="px-1 text-lg font-semibold rounded-lg cursor-pointer outline outline-4 outline-secondary-300 text-mc-orange">
-                  LinkedIn
-                </button>
-              </span>
-            )}
-          </a>
-          <div className="w-full">
-            <p className="mb-3 text-2xl font-bold text-quaternary-200">
-              Email: {user.email}
-            </p>
-            <p className="mb-3 text-2xl font-bold text-quaternary-200">
-              Expertise: {user.expertise}
-            </p>
-            <p className="mb-3 text-2xl font-bold text-quaternary-200">
-              Address: {user.address}
-            </p>
+            <a
+              href="https://www.linkedin.com/in/mcoskuncelebi/"
+              target="_blank"
+              rel="noreferrer"
+              className={`flex items-center text-lg md:text-xl lg:text-2xl font-semibold ${
+                entered
+                  ? "text-quaternary-300 underline cursor-pointer"
+                  : "text-mc-white"
+              }`}
+            >
+              {user.name}
+              {entered && (
+                <span className="px-2">
+                  <button className="px-1 text-base font-semibold rounded-lg cursor-pointer md:text-lg lg:text-xl outline outline-4 outline-secondary-300 text-mc-orange">
+                    LinkedIn
+                  </button>
+                </span>
+              )}
+            </a>
+            <div className="w-full">
+              <p className="mb-3 text-base font-bold md:text-lg lg:text-xl text-quaternary-200">
+                Email: {user.email}
+              </p>
+              <p className="mb-3 text-base font-bold md:text-lg lg:text-xl text-quaternary-200">
+                Expertise: {user.expertise}
+              </p>
+              <p className="mb-3 text-base font-bold md:text-lg lg:text-xl text-quaternary-200">
+                Address: {user.address}
+              </p>
+            </div>
           </div>
         </div>
       </div>
