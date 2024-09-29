180452fa4b675d4dbdec07bcfee22f6631002401
mcc1461 <mcoskuncelebi@gmail.com>
2024-09-29 08:59:54 +0300

    test: 2

--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
+// Import required modules
 const express = require("express");
-require("dotenv").config();
+const dotenv = require("dotenv"); // Correctly import dotenv
+const path = require("path"); // Import path module
+const cors = require("cors");
 const dbConnection = require("./config/dbConnection");
 const portfolioRoutes = require("./routes/portfolioRoutes");
 const authRoutes = require("./routes/authRoutes"); // Import auth routes
@@ -7,29 +10,29 @@ const visitorRoutes = require("./routes/visitorRoutes");
 
 // Determine the environment and load the corresponding .env file
 const env = process.env.NODE_ENV || "development";
-dotenv.config({ path: path.resolve(__dirname, `../../server/.env.${env}`) });
+dotenv.config({ path: path.resolve(__dirname, `.env.${env}`) }); // Properly configure dotenv
 
 const app = express();
-const cors = require("cors");
 const port = process.env.PORT || 8000;
 
+// Define allowed origins for CORS
 const allowedOrigins = [
   "http://127.0.0.1:3000",
   "https://127.0.0.1:3000",
   "https://musco.dev",
-  process.env.CLIENT_ORIGIN,
+  process.env.CLIENT_ORIGIN, // Ensure CLIENT_ORIGIN is defined in your .env files
 ];
 
-// server/src/index.js
+// Log CLIENT_ORIGIN for debugging purposes
 console.log("CLIENT_ORIGIN:", process.env.CLIENT_ORIGIN);
 
 // Database connection
 dbConnection();
 
-// Middleware
+// Middleware setup
 app.use(
   cors({
-    origin: allowedOrigins.filter(Boolean), // Remove undefined if CLIENT_ORIGIN is not set
+    origin: allowedOrigins.filter(Boolean), // Removes undefined if CLIENT_ORIGIN is not set
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true, // If you need to send cookies or auth headers
   })
@@ -39,7 +42,7 @@ app.use(express.json());
 
 app.use(express.static("public")); // Serve static files from the public folder
 
-// Routes
+// Define routes
 app.use("/api", portfolioRoutes);
 app.use("/api", authRoutes); // Add the auth routes
 app.use("/api", visitorRoutes);
@@ -50,6 +53,7 @@ app.get("/", (req, res) => {
 });
 
 // Start server
-app.listen(port, "127.0.0.1", () => {
+app.listen(port, "0.0.0.0", () => {
+  // Change binding address to 0.0.0.0
   console.log(`Server is running on port ${port}`);
 });
