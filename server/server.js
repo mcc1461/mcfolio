e413d1d6fa84d257f46e3fff482c291da59fa269
mcc1461 <mcoskuncelebi@gmail.com>
2024-09-29 12:59:36 +0300

    fix: env file

--- server/server.js
+++ server/server.js
@@ -3,19 +3,25 @@
 // Import required modules
 const dotenv = require("dotenv"); // Import dotenv
 const path = require("path"); // Import path module
+
+// Determine the environment and load the corresponding .env file
+const envFile =
+  process.env.NODE_ENV === "production"
+    ? ".env.production"
+    : ".env.development";
+
+// Configure dotenv to load environment variables from the specified file
+dotenv.config({ path: path.resolve(__dirname, envFile) });
+
+// Continue with other imports
 const express = require("express");
 const cors = require("cors");
 const dbConnection = require("./config/dbConnection");
 const portfolioRoutes = require("./routes/portfolioRoutes");
-const authRoutes = require("./routes/authRoutes"); // Import auth routes
+const authRoutes = require("./routes/authRoutes"); // If applicable
 const visitorRoutes = require("./routes/visitorRoutes");
 
-// Determine the environment and load the corresponding .env file
-const env = process.env.NODE_ENV || "development";
-
-// Load environment variables from the appropriate .env file
-dotenv.config({ path: path.resolve(__dirname, `.env.production`) });
-
+// Initialize Express app
 const app = express();
 const port = process.env.PORT || 8000;
 
@@ -24,10 +30,10 @@ const allowedOrigins = [
   "http://127.0.0.1:3000",
   "https://127.0.0.1:3000",
   "https://musco.dev",
-  process.env.CLIENT_ORIGIN, // Ensure CLIENT_ORIGIN is defined in your .env files
-].filter(Boolean); // Removes undefined if CLIENT_ORIGIN is not set
+  process.env.CLIENT_ORIGIN, // Ensure CLIENT_ORIGIN is defined
+].filter(Boolean); // Removes undefined entries
 
-// Log CLIENT_ORIGIN for debugging purposes
+// Log CLIENT_ORIGIN for debugging
 console.log("CLIENT_ORIGIN:", process.env.CLIENT_ORIGIN);
 
 // Database connection
@@ -43,12 +49,11 @@ app.use(
 );
 
 app.use(express.json());
-
-app.use(express.static("public")); // Serve static files from the public folder
+app.use(express.static("public")); // Serve static files
 
 // Define routes
 app.use("/api", portfolioRoutes);
-app.use("/api", authRoutes); // Add the auth routes
+app.use("/api", authRoutes); // If applicable
 app.use("/api", visitorRoutes);
 
 // Default route
@@ -58,6 +63,5 @@ app.get("/", (req, res) => {
 
 // Start server
 app.listen(port, "0.0.0.0", () => {
-  // Change binding address to 0.0.0.0
   console.log(`Server is running on port ${port}`);
 });
