ee20245f629cde2cb351d4b1ba2aa8b4656cd9fd
mcc1461 <mcoskuncelebi@gmail.com>
2024-09-29 08:46:50 +0300

    test: Testing URL issue

--- server/server.js
+++ server/server.js
@@ -5,6 +5,10 @@ const portfolioRoutes = require("./routes/portfolioRoutes");
 const authRoutes = require("./routes/authRoutes"); // Import auth routes
 const visitorRoutes = require("./routes/visitorRoutes");
 
+// Determine the environment and load the corresponding .env file
+const env = process.env.NODE_ENV || "development";
+dotenv.config({ path: path.resolve(__dirname, `../../server/.env.${env}`) });
+
 const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 8000;
@@ -13,7 +17,11 @@ const allowedOrigins = [
   "http://127.0.0.1:3000",
   "https://127.0.0.1:3000",
   "https://musco.dev",
+  process.env.CLIENT_ORIGIN,
 ];
+
+// server/src/index.js
+console.log("CLIENT_ORIGIN:", process.env.CLIENT_ORIGIN);
 
 // Database connection
 dbConnection();
@@ -21,8 +29,9 @@ dbConnection();
 // Middleware
 app.use(
   cors({
-    origin: allowedOrigins,
-    methods: ["GET", "POST"],
+    origin: allowedOrigins.filter(Boolean), // Remove undefined if CLIENT_ORIGIN is not set
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    credentials: true, // If you need to send cookies or auth headers
   })
 );
 
