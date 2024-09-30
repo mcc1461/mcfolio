ebdccf33c7cd4df6ebfcfe906b4f34bbad99102a
mcc1461 <mcoskuncelebi@gmail.com>
2024-09-30 08:20:39 +0300

    update: Header2

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,8 +3,8 @@ import VisitorCounter from "./VisitorCounter"; // Adjust the path if necessary
 
 const Header = () => {
   return (
-    <div className="relative p-3 h-[7vh] bg-mc-blue-darker1 flex justify-center items-center border-b-2 border-lime-300 z-50">
-      <h1 className="p-3 text-2xl font-bold leading-tight text-center gradient-text sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl">
+    <div className="fixed top-0 left-0 z-50 flex items-center justify-center w-full p-3 border-b-2 bg-mc-blue-darker1 border-lime-300">
+      <h1 className="p-3 text-lg font-bold leading-tight text-center gradient-text sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl">
         MusCo Portfolio
       </h1>
       <VisitorCounter />
