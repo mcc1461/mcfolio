49d29ab75e77657bed047bd963958c33de84fdec
mcc1461 <mcoskuncelebi@gmail.com>
2024-09-30 08:14:15 +0300

    update: Header is studied again

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,11 +1,10 @@
-// Header.js
 import React from "react";
 import VisitorCounter from "./VisitorCounter"; // Adjust the path if necessary
 
 const Header = () => {
   return (
     <div className="relative p-3 h-[7vh] bg-mc-blue-darker1 flex justify-center items-center border-b-2 border-lime-300 z-50">
-      <h1 className="h-full p-3 mt-2 text-xl font-bold gradient-text sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl">
+      <h1 className="p-3 text-2xl font-bold leading-tight text-center gradient-text sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl">
         MusCo Portfolio
       </h1>
       <VisitorCounter />
