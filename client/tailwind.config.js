8eaceabc2e98ea1ae63dcf276961263286fa506d
mcc1461 <mcoskuncelebi@gmail.com>
2024-10-02 00:17:30 +0300

    .

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -244,28 +244,19 @@ module.exports = {
     },
 
     screens: {
-      xsss: { min: "0px", max: "160px" },
-      // => @media (min-width: 640px and max-width: 767px) { ... }
+      sm: { max: "640px" },
+      // => @media (min-width: 640px) { ... }
 
-      xss: { min: "161px", max: "320px" },
-      // => @media (min-width: 640px and max-width: 767px) { ... }
+      md: { min: "641px", max: "767px" },
+      // => @media (min-width: 768px) { ... }
 
-      xs: { min: "321px", max: "640px" },
-      // => @media (min-width: 640px and max-width: 767px) { ... }
+      lg: { min: "768px", max: "1023px" },
+      // => @media (min-width: 1024px) { ... }
 
-      sm: { min: "641px", max: "767px" },
-      // => @media (min-width: 640px and max-width: 767px) { ... }
+      xl: { min: "1024px", max: "1279px" },
+      // => @media (min-width: 1280px) { ... }
 
-      md: { min: "768px", max: "1023px" },
-      // => @media (min-width: 768px and max-width: 1023px) { ... }
-
-      lg: { min: "1024px", max: "1279px" },
-      // => @media (min-width: 1024px and max-width: 1279px) { ... }
-
-      xl: { min: "1280px", max: "1535px" },
-      // => @media (min-width: 1280px and max-width: 1535px) { ... }
-
-      xl2: { min: "1536px" },
+      xl2: { min: "1280px" },
       // => @media (min-width: 1536px) { ... }
     },
   },
