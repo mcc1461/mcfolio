db705dd1cda8c67bf0f33a07f175e3ab3da6cc14
mcc1461 <mcoskuncelebi@gmail.com>
2024-10-02 12:46:49 +0300

    .

--- client/src/pages/Home/Experiences.js
+++ client/src/pages/Home/Experiences.js
@@ -4,8 +4,6 @@ import SectionTitle from "../../components/SectionTitle";
 
 const Experiences = () => {
   const [hoveredId, setHoveredId] = useState(null);
-
-  // Correctly access the experiences array from portfolioData
   const { portfolioData } = useSelector((state) => state.root);
   const experiences = portfolioData?.experiences || [];
 
@@ -14,16 +12,13 @@ const Experiences = () => {
       <SectionTitle title="Experiences" />
       <div className="flex flex-col items-center justify-center h-full overflow-hidden bg-mc-blue">
         <div className="flex flex-col w-full max-w-6xl px-4">
-          {" "}
-          {/* Added `px-4` for padding */}
           {experiences.map((experience) => (
             <div
-              key={experience._id} // Unique key for each experience using _id
-              className="flex flex-col items-center justify-center w-full p-4 m-4 mr-8 rounded-lg shadow-lg gap-7 bg-mc-blue-darker1"
+              key={experience._id}
+              className="flex flex-col items-center justify-center w-[calc(100%-32px)] p-4 m-4 mr-8 rounded-lg shadow-lg gap-7 bg-mc-blue-darker1"
               onMouseEnter={() => setHoveredId(experience._id)}
               onMouseLeave={() => setHoveredId(null)}
             >
-              {/* Period and Location Section */}
               <div
                 className={`w-full border-l-4 pl-2 cursor-pointer ${
                   hoveredId === experience._id
@@ -32,7 +27,7 @@ const Experiences = () => {
                 }`}
               >
                 <h2
-                  className={`text-xl sm:text-2xl md:text-3xl lg:text-3xl xl:text-4xl 2xl:text-4xl font-semibold ${
+                  className={`text-xl sm:text-2xl lg:text-3xl xl:text-4xl font-semibold ${
                     hoveredId === experience._id
                       ? "text-quaternary-300"
                       : "text-mc-white"
@@ -40,29 +35,25 @@ const Experiences = () => {
                 >
                   {experience.period}
                 </h2>
-                <div className="flex flex-col text-lg">
-                  {experience.location.map((location, index) => (
-                    <p
-                      key={`${experience._id}-location-${index}`} // Unique key combining _id and index for locations
-                      className={`text-sm sm:text-base md:text-lg lg:text-lg xl:text-xl 2xl:text-xl ${
-                        hoveredId === experience._id
-                          ? "text-secondary-300 font-semibold"
-                          : "text-secondary-100"
-                      }`}
-                    >
-                      {location}
-                    </p>
-                  ))}
-                </div>
+                {experience.location.map((location, index) => (
+                  <p
+                    key={`${experience._id}-${index}`}
+                    className={`text-sm sm:text-base lg:text-lg xl:text-xl ${
+                      hoveredId === experience._id
+                        ? "text-secondary-300 font-semibold"
+                        : "text-secondary-100"
+                    }`}
+                  >
+                    {location}
+                  </p>
+                ))}
               </div>
-
-              {/* Role and Description Section */}
               <div className="w-full">
-                <p className="mb-3 text-lg font-bold sm:text-xl md:text-2xl lg:text-2xl xl:text-3xl 2xl:text-3xl text-quaternary-200">
+                <p className="mb-3 text-lg font-bold sm:text-xl lg:text-2xl xl:text-3xl text-quaternary-200">
                   {experience.role}
                 </p>
                 {hoveredId === experience._id && (
-                  <p className="text-sm text-justify transition-opacity duration-300 sm:text-base md:text-lg lg:text-lg xl:text-xl 2xl:text-xl text-quaternary-100">
+                  <p className="text-sm text-justify transition-opacity duration-300 sm:text-base lg:text-lg xl:text-xl text-quaternary-100">
                     {experience.desc}
                   </p>
                 )}
