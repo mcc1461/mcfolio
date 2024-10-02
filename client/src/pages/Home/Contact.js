99e2041bfe29a43f6430e50a4ae9664d4e3372cc
mcc1461 <mcoskuncelebi@gmail.com>
2024-10-02 21:23:35 +0300

    .

--- client/src/pages/Home/Contact.js
+++ client/src/pages/Home/Contact.js
@@ -171,7 +171,7 @@ const Contact = () => {
       <SectionTitle title="Contact" />
       <div className="grid items-center w-full grid-cols-2 py-10 gap-7 md:grid-cols-1 sm:grid-cols-1 justify-items-center bg-mc-blue">
         {/* Video Section */}
-        <div className="flex justify-end w-full border border-red-200">
+        <div className="flex justify-center w-full border border-red-200 lg:justify-end ">
           <video
             ref={videoRef}
             className="w-full lg:h-[40vh] h-[40vh] lg:rounded-lg rounded-none min-w-[350px] max-w-[500px]  justify-end"
