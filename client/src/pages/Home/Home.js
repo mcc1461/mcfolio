0194c7a2f79ed79180a8d675a11e21fa6ce08512
mcc1461 <mcoskuncelebi@gmail.com>
2024-10-03 22:07:29 +0300

    .

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -55,7 +55,7 @@ function Home() {
       {/* Admin Logout Warning */}
       {isAdminLoggedIn && (
         <div
-          className="fixed top-[92px] right-0 z-50 px-4 py-2 text-white bg-red-600"
+          className="fixed top-[10%] right-5 z-50 px-4 py-2 text-white bg-red-600 rounded-lg shadow-lg"
           style={{ top: `${headerHeight}px` }} // Dynamically positioned based on header height
         >
           <span className="text-sm md:text-base">
