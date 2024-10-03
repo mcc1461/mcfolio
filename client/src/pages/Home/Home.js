efa0611d0731d1859f46e08be8f5065fc3d72a12
mcc1461 <mcoskuncelebi@gmail.com>
2024-10-03 20:16:34 +0300

    .

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -45,7 +45,7 @@ function Home() {
       {/* Admin Logout Warning */}
       {isAdminLoggedIn && (
         <div
-          className="fixed top-0 right-0 z-50 px-4 py-2 text-white bg-red-600"
+          className="fixed z-50 px-4 py-2 text-white bg-red-600 top-10 right-10"
           style={{ top: `${headerHeight}px` }}
         >
           <span>
