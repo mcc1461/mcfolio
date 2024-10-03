ce298c62027f77c2fabc0e0040012c76064ddbe8
mcc1461 <mcoskuncelebi@gmail.com>
2024-10-03 19:09:10 +0300

    .

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -44,27 +44,6 @@ function Home() {
         <Header />
       </div>
 
-      {/* Notification Bar */}
-      <div
-        className="fixed left-0 right-0 z-50 p-4 text-center text-white bg-blue-600"
-        style={{ top: `${headerHeight}px` }} // Dynamically position below the header
-      >
-        {isAdminLoggedIn ? (
-          <p className="font-bold">
-            You are still logged in as Admin! Please{" "}
-            <span
-              onClick={handleLogout}
-              className="underline cursor-pointer hover:text-gray-300"
-            >
-              log out
-            </span>{" "}
-            for security reasons.
-          </p>
-        ) : (
-          <p className="font-bold">Welcome to the Website!</p>
-        )}
-      </div>
-
       {/* Main Content */}
       <div style={{ marginTop: `${headerHeight + 64}px` }}>
         {" "}
