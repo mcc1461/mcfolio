62adcb5d5d721d55c9b25154e67c68b3a2697a2a
mcc1461 <mcoskuncelebi@gmail.com>
2024-10-04 12:11:40 +0300

    .

--- server/server.js
+++ server/server.js
@@ -1,13 +1,8 @@
+// server.js
+
 // Import required modules
 const dotenv = require("dotenv"); // Import dotenv
 const path = require("path"); // Import path module
-const express = require("express");
-const cors = require("cors");
-const mongoose = require("mongoose"); // Ensure mongoose is imported
-const portfolioRoutes = require("./routes/portfolioRoutes");
-const adminRoutes = require("./routes/adminRoutes"); // Adjusted to include admin routes
-const visitorRoutes = require("./routes/visitorRoutes");
-const createError = require("http-errors"); // To handle 404 errors
 
 // Determine the environment and load the corresponding .env file
 const envFile =
@@ -17,6 +12,14 @@ const envFile =
 
 // Configure dotenv to load environment variables from the specified file
 dotenv.config({ path: path.resolve(__dirname, envFile) });
+
+// Continue with other imports
+const express = require("express");
+const cors = require("cors");
+const dbConnection = require("./config/dbConnection");
+const portfolioRoutes = require("./routes/portfolioRoutes");
+const authRoutes = require("./routes/authRoutes"); // If applicable
+const visitorRoutes = require("./routes/visitorRoutes");
 
 // Initialize Express app
 const app = express();
@@ -34,20 +37,6 @@ const allowedOrigins = [
 console.log("CLIENT_ORIGIN:", process.env.CLIENT_ORIGIN);
 
 // Database connection
-const dbConnection = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("Database connected successfully");
-  } catch (error) {
-    console.error("Database connection failed:", error);
-    process.exit(1); // Exit the process with failure
-  }
-};
-
-// Call database connection function
 dbConnection();
 
 // Middleware setup
@@ -64,7 +53,7 @@ app.use(express.static("public")); // Serve static files
 
 // Define routes
 app.use("/api", portfolioRoutes);
-app.use("/api", adminRoutes); // Admin routes are defined here
+app.use("/api", authRoutes); // If applicable
 app.use("/api", visitorRoutes);
 
 // Default route
@@ -72,24 +61,7 @@ app.get("/", (req, res) => {
   res.send("Hello World from MusCo Portfolio API");
 });
 
-// Handle 404 errors for unknown routes
-app.use((req, res, next) => {
-  next(createError(404, "Route not found"));
-});
-
-// Global error handling middleware
-app.use((err, req, res, next) => {
-  // Log the error for debugging
-  console.error(err.stack);
-
-  // Customize the error response
-  res.status(err.status || 500).json({
-    success: false,
-    message: err.message || "Internal Server Error",
-  });
-});
-
-// Start the server
+// Start server
 app.listen(port, "0.0.0.0", () => {
   console.log(`Server is running on port ${port}`);
 });
